44d978055bf2ea156a80624860f1ff6380363684
Dzer0 <23442435@qq.com>
2017-05-06 05:48:50 +0800

    增加检查是否更新功能。

---
+++ anqingtv_local.py
@@ -0,0 +1,110 @@
+# conding:utf-8
+'''
+pip install oss2
+pip install MySQL-python
+'''
+import MySQLdb
+import oss2
+import urlparse
+import os
+import urllib2
+import requests
+
+oss_accesskey = 'LTAIQ0tsW1ptho2c'
+oss_accesskey_secret = 'rfFO1jcWWcSmL1HhPpbf7SyZFMIXb1'
+oss_bucket_name = 'zhizhebuyan'
+
+mysqlUser = 'bdm260467543'
+mysqlPwd = 'yuming01'
+mysqlUrl = 'bdm260467543.my3w.com'
+mysqlPort = '3306'
+mysqlDatabasename = 'bdm260467543_db'
+
+def Upload_oss_get_url(image_url,id):
+    '''
+        img_name = images name
+        path = Image absolute path + image file name
+    '''
+    headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; \
+    en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+    # no support china
+    if urlparse.urlparse(image_url).query :
+        new_url = urlparse.urlparse(image_url).query.split('=')[1]
+        img_name = 'anqingtv.com' + urlparse.urlparse(new_url).path
+    else:
+        img_name = 'anqingtv.com' + urlparse.urlparse(image_url).path
+    endpoint = 'http://oss-cn-hangzhou.aliyuncs.com'
+    auth = oss2.Auth(oss_accesskey, oss_accesskey_secret)
+    bucket = oss2.Bucket(auth, endpoint, oss_bucket_name)
+    #oss2.resumable_upload(bucket, img_name, path)
+    input = requests.get(image_url ,headers = headers,timeout=5)
+    bucket.put_object(img_name, input)
+    return True
+
+def GetMySQLdbInfo(mysqlUrl,mysqlUser,mysqlPwd,mysqlDatabasename):
+    db = MySQLdb.connect(mysqlUrl,mysqlUser,mysqlPwd,mysqlDatabasename)
+    cursor = db.cursor()
+    #Get old id
+    get_old_id_sql = 'select * from history where od=1'
+    cursor.execute(get_old_id_sql)
+    old_id = cursor.fetchall()[0][0]
+    print old_id
+    # get new artile id
+    get_new_id_sql = 'select * from mac_vod ORDER BY d_id DESC LIMIT 1'
+    cursor.execute(get_new_id_sql)
+    new_id = cursor.fetchall()[0][0]
+    print new_id
+    if old_id >=new_id:
+        results = False
+    else:
+        # get all imgurl sql
+        #get_all_imgurl_sql = 'select * from mac_vod LIMIT ' + \
+        #str(old_id) + ',' + str(new_id)+';'
+        get_all_imgurl_sql = 'select * from mac_vod where d_id > %s ' %str(old_id)
+        print get_all_imgurl_sql
+        cursor.execute(get_all_imgurl_sql)
+        results = cursor.fetchall()
+        # update old id
+        update_old_id_sql = 'update history set old_id=%s where od=1' %new_id
+        cursor.execute(update_old_id_sql)
+    db.commit()
+    db.close()
+    return results
+
+def UpdateDataBase(mysqlUrl,mysqlUser,mysqlPwd,mysqlDatabasename,new_img_url,new_d_id):
+    db = MySQLdb.connect(mysqlUrl,mysqlUser,mysqlPwd,mysqlDatabasename)
+    cursor = db.cursor()
+    try:
+        #update databse SQL
+        sql = 'update mac_vod set d_pic ="%s" where d_id =%s' %(new_img_url,new_d_id)
+        print sql
+        cursor.execute(sql)
+        db.commit()
+        db.close()
+    except Exception as e:
+        print e
+    return 'success'
+
+
+if __name__ == '__main__':
+    print 'Python Run Path %s' % os.getcwd()
+    results = GetMySQLdbInfo(mysqlUrl,mysqlUser,mysqlPwd,mysqlDatabasename)
+    if results:
+        for i in results:
+            if i[6]:
+                print i[0],i[6]
+                #DownloadImage_updatedatabse(i[0],i[6])
+                try:
+                    if 'oss.zhizhebuyan.com' in i[6]:
+                        print('%s the updated.') %i[6]
+                    else:
+                        Upload_oss_get_url(i[6],i[0])
+                        new_img_url = 'http://oss.zhizhebuyan.com/anqingtv.com' + str(urlparse.urlparse(i[6]).path)
+                        print new_img_url
+                        UpdateDataBase(mysqlUrl,mysqlUser,mysqlPwd,mysqlDatabasename,new_img_url,i[0])
+                except Exception as e:
+                    print e
+            else:
+                print 'pic no found'
+    else:
+        print 'No update moive End!!!'
